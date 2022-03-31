414d9ef0903552a554ab218cbebde704c20a94f5
Gsn1997 <gouthamsn123@gmail.com>
2022-03-31 23:14:46 +0530

    commit

---
+++ src/pastries_mock.ts
@@ -0,0 +1,10 @@
+export const pastries=[
+    // {name:'Alexandertorte',path:'../../assets/1.jpg'},
+    {name:'Alfajor',path:'../../assets/2.jpg'},
+    {name:'Applepie',path:'../../assets/3.jpg'},
+    {name:'Apple strudel',path:'../../assets/4.jpg'},
+    // {name:'Bahulu',path:'../../assets/5.jpg'},
+    {name:'Bakpia Pathok',path:'../../assets/6.jpg'},
+]
+
+   
